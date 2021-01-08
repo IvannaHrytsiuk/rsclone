eac36035c03677e4ee2a703a085292a25e81c1b0
Roman <gupalenko.98@mail.ru>
2021-01-08 17:02:12 +0300

    feat: add work with api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
 // import './styles/style.scss';
+// eslint-disable-next-line import/extensions
+import './js/covid-map/covid-map.js';
--- src/js/covid-map/covid-map.js
+++ src/js/covid-map/covid-map.js
@@ -1,1 +1,6 @@
-// Здесь будет рендериться, выводиться карта и выполняться переключение
+// eslint-disable-next-line import/extensions
+import getGeoJSON from './covid-api.js';
+
+document.querySelector('#countries').addEventListener('change', (e) => {
+  getGeoJSON(e.target.value);
+});
