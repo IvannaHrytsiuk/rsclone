2b9b7e46fe831ec82f4c79d13849f2e9c6e77da0
Roman <gupalenko.98@mail.ru>
2021-02-01 20:25:34 +0300

    gh-pages/feature: add a folder with the project build

--- src/scripts/main-styles.js
+++
@@ -1,5 +0,0 @@
-import '../styles/nullStyle.scss';
-import '../styles/style.scss';
-import '../assets/map/leaflet.css';
-import '../assets/fullscreen/Control.FullScreen.css';
-import '../styles/covid-map.scss';
--- src/scripts/other-styles.js
+++
@@ -1,2 +0,0 @@
-import '../styles/nullStyle.scss';
-import '../styles/style.scss';
