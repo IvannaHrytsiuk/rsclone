6bc2c3ce4989af7d88bc6d911f1ecb77bed48dca
Roman <gupalenko.98@mail.ru>
2021-03-04 11:32:18 +0300

    develop/feature: update heroku servers source code

--- heroku-servers/airport/index.js
+++ heroku-servers/airport/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const request = require('request');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
@@ -11,21 +12,28 @@ app.use((req, res, next) => {
 
 app.get('/:id', (req, res, next) => {
     const { id } = req.params;
-    request({
-        uri: `https://www.skyscanner.co.th/g/autosuggest-flights/BY/en-US/${id}`,
-        followAllRedirects: true,
-        method: 'GET',
-        qs: {
-            isDestination: 'false',
-            enable_general_search_v2: 'true',
-        },
-        headers: {
-            'User-Agent': 'PostmanRuntime/7.26.8',
-            'Content-Type': 'application/json; charset=utf-8',
-        },
-    }).pipe(res);
+
+    if (id && id !== 'favicon.ico') {
+        request({
+            uri: `https://www.skyscanner.co.th/g/autosuggest-flights/BY/en-US/${id}`,
+            followAllRedirects: true,
+            method: 'GET',
+            qs: {
+                isDestination: 'false',
+                enable_general_search_v2: 'true',
+            },
+            headers: {
+                'User-Agent': 'PostmanRuntime/7.26.8',
+                'Content-Type': 'application/json; charset=utf-8',
+            },
+        }).pipe(res);
+    }
 });
 
-app.listen(process.env.PORT, () => {
-    console.log(`Listen as PORT: ${process.env.PORT}`);
+app.get('*', (req, res) => {
+    res.sendStatus(404);
+});
+
+app.listen(PORT, () => {
+    console.log(`Listen as PORT: ${PORT}`);
 });
--- heroku-servers/geojson/index.js
+++ heroku-servers/geojson/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const request = require('request');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
@@ -11,23 +12,30 @@ app.use((req, res, next) => {
 
 app.get('/:id', (req, res) => {
     const { id } = req.params;
-    request({
-        uri: 'https://www.skyscanner.co.th/g/can-i-go-map-api/map/feature-collection-translated',
-        followAllRedirects: true,
-        method: 'GET',
-        qs: {
-            isMobile: 'false',
-            locale: 'en-US',
-            market: 'BY',
-            originId: id,
-        },
-        headers: {
-            'User-Agent': 'PostmanRuntime/7.26.8',
-            'Content-Type': 'application/json; charset=utf-8',
-        },
-    }).pipe(res);
+
+    if (id && id !== 'favicon.ico') {
+        request({
+            uri: 'https://www.skyscanner.co.th/g/can-i-go-map-api/map/feature-collection-translated',
+            followAllRedirects: true,
+            method: 'GET',
+            qs: {
+                isMobile: 'false',
+                locale: 'en-US',
+                market: 'BY',
+                originId: id,
+            },
+            headers: {
+                'User-Agent': 'PostmanRuntime/7.26.8',
+                'Content-Type': 'application/json; charset=utf-8',
+            },
+        }).pipe(res);
+    }
 });
 
-app.listen(process.env.PORT, () => {
-    console.log(`Listen as PORT: ${process.env.PORT}`);
+app.get('*', (req, res) => {
+    res.sendStatus(404);
+});
+
+app.listen(PORT, () => {
+    console.log(`Listen as PORT: ${PORT}`);
 });
